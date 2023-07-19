10081cabcd4faf10c087e22ecd0671dc247ee74b
hasna <manelhasnahaddoud@gmail.com>
2023-07-19 20:12:09 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Follow along with the examples here
+/*function sayHello() {
+    if(true){
+        console.log("Hello!");
+
+
+    }
+  }
+  sayHello();
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+function doSomething(x) {
+    console.log(x);
+  }
+  
+  doSomething("anything"); */
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+    //    console.log(greeting, firstName  );
+
+  }
+  say("Julio", "hello");
+  
+
+
+  function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`;
+    //    console.log(greeting, firstName  );
+
+  }
+     console.log(say("Julio", "hello"));
+
+
+
